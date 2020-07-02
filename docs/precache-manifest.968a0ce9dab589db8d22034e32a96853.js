f8c1ecb5912407908fcb95fe95ac319f39aa05c9
Anastasiya Alekseeva <anastasiya.alekseeva@simbirsoft.com>
2020-07-02 13:06:29 +0400

    init

---
+++ docs/precache-manifest.968a0ce9dab589db8d22034e32a96853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef786ab0f0d1c2f380dfe390a7ea8ad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caf510829a8f3668f64c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e94b4886a1ec31881c6",
+    "url": "/static/js/2.f32d06e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f32d06e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf510829a8f3668f64c",
+    "url": "/static/js/main.000d2c4e.chunk.js"
+  },
+  {
+    "revision": "c46a6e1d4e0d4d5f8296",
+    "url": "/static/js/runtime-main.a0fee805.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
